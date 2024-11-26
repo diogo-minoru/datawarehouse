c2777fcf56b6fda51ae353f00a824bcfff3742ae
diogo-minoru <132161289+diogo-minoru@users.noreply.github.com>
2024-11-26 20:22:21 -0300

    criando staging da tabela

---
+++ datawarehouse/models/staging/stg.commodities.sql
@@ -0,0 +1,18 @@
+with source as(
+    select 
+        "Date",
+        "Close",
+        simbolo
+    from {{source ("postgres", "commodities")}}
+),
+
+renamed as(
+    select
+        cast("Date" as date) as data,
+        "Close" as valor_fechamento,
+        simbolo
+    from source
+)
+
+select *
+from renamed
